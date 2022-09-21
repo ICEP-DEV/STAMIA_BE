4648c1bdb4cb8e1210336d44d53216330dd8663c
Karabopleasure <karabopleasure18@gmail.com>
2022-09-21 12:59:16 -0700

    add file

--- controller/quizController.js
+++ controller/quizController.js
@@ -20,94 +20,31 @@ var questions=[];
     const quizName = req.body.quizName;
     const subjectName = req.body.subjectName;
     const question = req.body.question;
-  //   const randomValue = (list) => {
-  //     return list[Math.floor(Math.random() * list.length)];
-  // };
 
   const quiz  = await quizModel.findAll({attributes: ["quizName", "subjectName" , "question"],
   where: ({quizName: quizName})})
- //let myquiz;
- for(var i = 0; i < quiz.length; i++){
-
-     var random = Math.floor(Math.random() * quiz.length);
-     
-     questions.push(random);
- }
-
- console.log(questions);
-// res.status(200).json(myquiz)
-
-// const values = Object.values(quiz)
-
-// const randomValue = values[parseInt(Math.random() * values.length )]
-
-//if(questions.find(randomValue)== -1 ){
 
 
-   console.log(JSON.stringify(quiz));
-   console.log(index)
-   index++;
-      res.status(202).send(quiz[questions[index]]);
- 
+do {
+    // Generating random number
+    const randomNumber = Math.floor(Math.random() * quiz.length)
   
+    // Pushing into the array only 
+    // if the array does not contain it
+    if (!questions.includes(randomNumber)) {
+        questions.push(randomNumber);
+    }
+  
+} while (questions.length < quiz.length);
 
-//}else{
+//console.log(questions)
+//console.log(quiz[questions[index]])
+res.status(202).json(quiz[questions[index]]);
 
-
-//}
-
-
-//res.status(200).json(randomValue)
-
-   // while(quiz.length){
-    //   var rand = (quiz.splice(quiz.length * Math.random() | 0, 1)[0] )
-    // }
-
-    // if(quiz.indexOf(quiz[i])=== -1 && quiz[i] !== ''){
-    //    myquiz.push(quiz[i]);
-
-    //    return myquiz;
-    // }  
-
-//  console.log(rand)
-//  res.status(200).json(rand)
-
-
-     //   res.status(200).json(randomValue(quiz));
-
-        // var quizzes = quiz.slice();
-        // while(quizzes.length){
-           
-        //   random = Math.floor(Math.random() * quizzes.length);
-        //   console.log(quizzes[random]);
-        //   quizzes[random] = ' ';
-        //   quizzes = quizzes.filter(a=>{return a});
-        // }
-
-        // console.log(quiz);
-        
-      //   function* randomValue(arr){
-      //    let value;
-      //    while(arr.length > 0){
-      //       let ques = Math.floor(Math.random() * arr.length);
-      //       value = arr.splice(ques,1)[0];
-      //       yield value;
-
-      //    }
-         
-      //   }
-      //   res.status(200).json(randomValue(quiz));
-        //const select = quiz.sort(() => Math.random() - 0.5).slice(0, 5)
-       // res.status(200).json(select);
-   //          res.status(200).json({
-   //      status:"success",
-   //      message:"quiz selected",
-   //      quiz             
-   //  })
-
-   
+//console.log(index)
+   index++
+  
 })
-
 
     //UPDATE QUIZ
     exports.updateQuiz = catchAsync(async(req, res, next)=>{
@@ -124,8 +61,6 @@ var questions=[];
          
         }
 
-
-   
           const updateAnswer  = await quizModel.update({learnerAnswer: learnerAnswer, mark:score},
        {where: ({quizName: quizName, subjectName:subjectName, question: question, answer:answer})})
     
