978e9a59457fb04ce1cf8f9ffb6d4ee9843b80e9
Mosefowa KV <107556868+Vilcosia@users.noreply.github.com>
2022-09-21 11:10:34 +0200

    Update learnerRoute.js

--- routes/learnerRoute.js
+++ routes/learnerRoute.js
@@ -5,5 +5,8 @@ const learnerController = require('../controller/learnerController');
 router
     .route("/")
     .post(learnerController.createLearner)
+router.get('/', function(req, res, next) {
+        res.send('respond with a resource');
+      })
 
 module.exports = router;
