e2129d806cdf43029a5fabae03836e43eb6b3f4c
Mosefowa KV <107556868+Vilcosia@users.noreply.github.com>
2022-09-26 09:52:45 +0200

    Update loginRoute.js

--- routes/loginRoute.js
+++ routes/loginRoute.js
@@ -5,5 +5,9 @@ const loginController = require('../controller/loginController');
 router
     .route("/")
     .post(loginController.compareLogin)
+    .get(loginController.getlogin)
+    router.get('/', function(req, res, next) {
+        res.send('Welcome to STEMIA!!');
+      })
 
 module.exports = router;
