b9db22b8d61ad873141c60f10835e5a5408fd6ea
Karabopleasure <karabopleasure18@gmail.com>
2022-10-18 11:22:25 +0200

    update page

--- controller/learnerController.js
+++ controller/learnerController.js
@@ -1,4 +1,4 @@
- //CRUD - Create, Read, Update Delete
+//CRUD - Create, Read, Update Delete
 const learnerModel = require('../models/learnerModel');
 const catchAsync = require('../utils/catchAsync');
 
@@ -12,18 +12,32 @@ exports.createLearner = catchAsync(async(req, res, next)=>{
         learner
     })
 });
-
-
-
 exports.getLearner = catchAsync(async(req, res, next)=>{
-    const email = req.body.email;
-    const password = req.body.password;
-    const learner  = await learnerModel.findOne({
-        where: ({email: email },{password: password}),
-    });
-    res.status(200).json({
-        status:"success",
-        message:"learner selected",
-        learner
-    })
+    const learner  = await learnerModel.findAll()
 })
+
+//update new password if a learner reset password
+exports.updatePassword = catchAsync(async(req, res, next)=>{
+    const password = req.body.password;
+    const email = req.body.email;
+
+        const email1  = await learnerModel.findOne({attributes: ["email"],
+            where: ({email:email}) 
+        });
+    
+     if(email1){
+    
+        const updatePassword  = await learnerModel.update({password: password},
+            {where: ({email: email})})
+         
+              res.status(200).json({
+                 status:"success",
+                 message:"password updated",
+                 updatePassword
+             })
+        
+     }else{
+        return res.status(200).json("Email does not exist")
+     }
+    
+    })
