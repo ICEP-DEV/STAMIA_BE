8302f7b3e58df1137943a106adbdfee07d239866
Mosefowa KV <107556868+Vilcosia@users.noreply.github.com>
2022-11-30 14:05:05 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 //1 REQUIRE PACKAGES
 const express = require('express');
-const morgan = require('morgan');
 const globalErrHandler = require('./utils/errHandler.js');
 const cors = require('cors');
+const cookieParser = require("cookie-parser");
+const sessions = require('express-session');
 
 
 
@@ -16,18 +17,31 @@ const subjectRoute = require('./routes/subjectRoute');
 const eventsRoute = require('./routes/eventsRoute');
 
 
-
 //3 APP.USE MIDDLEWARE
 app.use(express.json());
 app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
+// creating 24 hours from milliseconds
+const oneDay = 1000 * 60 * 60 * 24;
 
+//session middleware
+app.use(sessions({
+    secret: "thisismysecrctekeyfhrgfgrfrty84fwir767",
+    saveUninitialized:true,
+    cookie: { maxAge: oneDay },
+    resave: false
+}));
+
+app.use(express.static(__dirname));
 
 //4 CREATE API URL
-app.use('/api/learner/', learnerRoute);
+app.use('/api/learner', learnerRoute);
 app.use('/api/login', loginRoute);
 app.use('/api/subject',subjectRoute);
-app.use('/api/event', eventsRoute);
+app.use('/api/events', eventsRoute);
+
 
 
 
