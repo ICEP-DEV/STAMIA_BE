a4afaf8c0b29d49a72047876a1a30da672622e22
Mosefowa KV <107556868+Vilcosia@users.noreply.github.com>
2022-11-30 14:01:38 +0200

    Update loginController.js

--- controller/loginController.js
+++ controller/loginController.js
@@ -1,29 +1,39 @@
-//CRUD - Create, Read, Update Delete
-const loginModel = require('../models/learnerModel');
-const catchAsync = require('../utils/catchAsync');
-const express = require('express');
 
-const router =express.Router();
+const learnerModel = require('../models/learnerModel');
+const catchAsync = require('../utils/catchAsync');
+
 
 exports.compareLogin = catchAsync(async(req,res,next)=>{
     const {email,password}=req.body;
-    const LoginData = await loginModel.findLearnerById({where:{email}}).catch((err)=>{
+    const LoginData = await learnerModel.findOne({where:{email}, limit: 1}).catch((err)=>{
+        
     console.log("Error:",err);
+
 })
 
-if(!LoginData)
-return res.json({message:"LEARNER EMAIL OR PASSWORD DOES NOT MATCH "});
 
-if(LoginData.password !==password)
-return res.json({message:"LEARNER EMAIL OR PASSWORD DOES NOT MATCH "});
-else{
-    return res.json({message:"login was successfull "});
+    if(!LoginData)
+     return res.json({message:"LEARNER EMAIL "});
+
+    if(LoginData.password !==password)
+     return res.json({message:"LEARNER PASSWORD DOES NOT MATCH "});
+     else{
+    
+        if(req.body.email == LoginData.email && req.body.password == LoginData.password){
+           session=req.session;
+           session.email=req.body.email;
+           console.log(req.session)
+           res.send(`Hey there, welcome to STEMIA`);
+
+        }
+         else{
+          res.send('Invalid email or password');
+        }
+    
 }
 
 
 });
-
-
 
 exports.getlogin = catchAsync(async(req, res, next)=>{
  const LoginData  = await loginModel.findAll()
