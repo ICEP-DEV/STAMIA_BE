f318a44f3622877bad95cfb2cebcf7f44065672e
Mosefowa KV <107556868+Vilcosia@users.noreply.github.com>
2022-12-05 14:34:20 +0200

    Update eventsRoute.js

--- routes/eventsRoute.js
+++ routes/eventsRoute.js
@@ -4,7 +4,7 @@ const eventsController = require('../controller/eventsController');
 
 
 router.post(eventsController.createEvent);
-router.get('/', eventsController.getAll);
+router.get('/', eventsController.findAll);
 router.get('/:eventId', eventsController.getEventById);
 router.put('/:eventId', eventsController.updateEvent);
 router.delete('/:eventId', eventsController.deleteById);
